facaf1743f126bc7197881c6f716a6cf5d5ff994
tenyega <tenyega23@gmail.com>
2024-11-04 21:25:09 +0100

    footer and navbar integrated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(OrderRepository $or): Response
     {
+        $orders = $or->findBy([
+            'paymentStatus' => 'PAID',
+            'orderStatus' => 'PROCESSED'
+        ]);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'orders' => $orders,
         ]);
     }
 }
