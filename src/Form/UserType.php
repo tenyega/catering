595fa8851014d80aabdbd08a582601c045fa5568
tenyega <tenyega23@gmail.com>
2024-12-15 22:48:47 +0100

    added changing password option for the user under the profile submenu

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -14,7 +14,6 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password')
             ->add('firstName')
             ->add('lastName')
             ->add('phone')
