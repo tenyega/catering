0ed6dc013ae9a890c2732be65fbdfdb9e1c27667
tenyega <tenyega23@gmail.com>
2025-01-28 10:31:34 +0100

    delivery

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Controller;
+
 use App\Service\ReportService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 class AdminController extends AbstractController
@@ -17,7 +19,7 @@ class AdminController extends AbstractController
         $this->reportService = $reportService;
     }
 
-  
+
     #[Route('/admin', name: 'admin_report')]
     public function index(): Response
     {
@@ -30,6 +32,4 @@ class AdminController extends AbstractController
             'salesReport' => $salesReport,
         ]);
     }
-
-    
 }
